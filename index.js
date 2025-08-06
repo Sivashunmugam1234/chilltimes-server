16c737b4f1948859e7cb9605cb91e5d4522a9b92
Sivashunmugam1234 <137296263+Sivashunmugam1234@users.noreply.github.com>
2025-08-07 00:58:56 +0530

    authentication

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
+require('dotenv').config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const userModel=require('./models/UserModel')
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const jwt=require("jsonwebtoken");
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/chillTimes");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB connected'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
@@ -48,9 +59,28 @@ app.post("/chillTimes/login", async (req, res) => {
     if (!match) {
       return req.status(400).json({ error: "password wrong" });
     }
-
-    res.status(200).json({ message: "login success" });
+   
+    
+    const payload = {
+      userId: user.id,
+      username: user.userName, 
+      email: user.email
+    };
+    const token=jwt.sign(payload,SECRET_KEY,{
+      expiresIn:'48h'
+    });
+       res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        username: user.userName,
+        email: user.email
+      }
+    });
+    // res.status(200).json({ message: "login success" });
   } catch {
+    
     res.status(500).json({ error: "Internal server error" });
   }
 });
